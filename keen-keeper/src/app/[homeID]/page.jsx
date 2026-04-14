ad27de8969e027595e710f0d5f3bc831fc7a9f6d
md-kudrot-592 <kk.mdkudrot@gmail.com>
2026-04-14 20:17:15 +0600

    feat: update FriendCart component to fix fetch URL and enhance HomeID component with detailed friend display

--- keen-keeper/src/app/[homeID]/page.jsx
+++ keen-keeper/src/app/[homeID]/page.jsx
@@ -1,4 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
+import { FiVideo } from 'react-icons/fi';
+import { MdOutlineTextsms } from 'react-icons/md';
+import { RiNotificationSnoozeLine } from "react-icons/ri";
+import { RiArchiveLine } from "react-icons/ri";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { TbPhoneCalling } from 'react-icons/tb';
+
+
+
 
 const friendsPromise = async () => {
     const res = await fetch("http://localhost:3000//friends.json");
@@ -11,7 +21,7 @@ const HomeID = async ({ params }) => {
     const friends = await friendsPromise();
 
 
-    const { homeID }= await params;
+    const { homeID } = await params;
     console.log(homeID);
 
     const friend = friends.find(friend => friend.id === parseInt(homeID));
@@ -19,8 +29,106 @@ const HomeID = async ({ params }) => {
 
 
     return (
-        <div>
-            {friend.name}
+        <div className='bg-base-200'>
+            <div className="container w-[60%] mx-auto py-10 flex gap-4 p-4 ">
+
+                <div className=" w-[30%]  flex flex-col ">
+                    <div key={friend.id} className=" bg-white w-full rounded-lg shadow-md p-4 flex items-center flex-col gap-2">
+                        <Image
+                            src={friend.picture}
+                            alt={friend.name}
+                            width={100}
+                            height={100}
+                            className="rounded-full object-cover object-center w-[100px] h-[100px]"
+                        />
+                        <h3 className="text-xl font-semibold mb-2">{friend.name}</h3>
+                        <p className="text-gray-600">{friend.days_since_contact}d ago</p>
+                        <div className="flex items-center gap-2 flex-wrap justify-center ">
+                            {
+                                friend.tags.map((tag, index) => <button key={index} className="bg-[#CBFADB]  text-[#244D3F]  py-2 px-2 rounded-full text-sm">{tag}</button>)
+                            }
+                        </div>
+
+                        <button
+                            className={`${friend.status === 'overdue'
+                                ? 'bg-[#EF4444]'
+                                : friend.status === "almost due"
+                                    ? 'bg-[#EFAD44]'
+                                    : friend.status === 'on-track'
+                                        ? 'bg-[#244D3F]'
+                                        : ""
+                                } text-white py-2 px-2 rounded-full text-sm`}
+                        >
+                            {friend.status}
+                        </button>
+
+
+                    </div>
+                    <div className="flex flex-col gap-4 mt-10">
+                        <button className="bg-white  text-black  py-2 px-4 rounded-lg w-full flex gap-2 items-center justify-center"> <RiNotificationSnoozeLine /> Snooze 2 weeks</button>
+                        <button className="bg-white  text-black  py-2 px-4 rounded-lg w-full flex gap-2 items-center justify-center"> <RiArchiveLine />
+                            ArchiveArchive</button>
+                        <button className="bg-white  text-black  py-2 px-4 rounded-lg w-full flex gap-2 items-center justify-center"> <RiDeleteBinLine />
+                            Delete</button>
+                    </div>
+                </div>
+
+                <div className=" w-[70%]  flex flex-col gap-4 p-4">
+                    <div className="">
+                        <div className="container mx-auto     grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+
+                            <div className="bg-white p-2 py-10 rounded-lg flex flex-col items-center text-center">
+                                <h1 className='text-[#244D3F] text-2xl font-bold'>10</h1>
+                                <p className='text-[#64748B]'>Total friends</p>
+                            </div>
+                            <div className="bg-white p-2 py-10 rounded-lg flex flex-col items-center text-center">
+                                <h1 className='text-[#244D3F] text-2xl font-bold'>3</h1>
+                                <p className='text-[#64748B]'>On Track</p>
+                            </div>
+                            <div className="bg-white p-2 py-10 rounded-lg flex flex-col items-center text-center">
+                                <h1 className='text-[#244D3F] text-2xl font-bold'>6</h1>
+                                <p className='text-[#64748B]'>Need Attention</p>
+                            </div>
+                            <div className="bg-white p-2 py-10 rounded-lg flex flex-col items-center text-center">
+                                <h1 className='text-[#244D3F] text-2xl font-bold'>12</h1>
+                                <p className='text-[#64748B]'>Interactions This Month</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="">
+                        <div className="p-4 bg-white rounded-lg shadow-md">
+                            <div className="flex items-center justify-between ">
+                                <h1>Relationship Goal</h1>
+                                <button className='btn'>Edit</button>
+                            </div>
+                            <p className='text-gray-600 mt-2'>contact every <b>30</b> days</p>
+                        </div>
+                    </div>
+
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                        <h1 className='py-2'>Quick Check-In</h1>
+                        <div className="container mx-auto  grid grid-cols-1 md:grid-cols-3  gap-4">
+
+                            <div className="bg-gray-100 p-2 py-10 rounded-lg flex flex-col items-center text-center">
+                                <TbPhoneCalling />
+                                <p className='text-[#64748B]'>Total friends</p>
+                            </div>
+                            <div className="bg-gray-100 p-2 py-10 rounded-lg flex flex-col items-center text-center">
+                                <MdOutlineTextsms />
+                                <p className='text-[#64748B]'>On Track</p>
+                            </div>
+                            <div className="bg-gray-100 p-2 py-10 rounded-lg flex flex-col items-center text-center">
+                                <FiVideo />
+                                <p className='text-[#64748B]'>Need Attention</p>
+                            </div>
+                    
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     );
 };
--- keen-keeper/src/components/FriendCart.jsx
+++ keen-keeper/src/components/FriendCart.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 
 const friendsPromise = async () => {
-    const res = await fetch("http://localhost:3000//friends.json");
+    const res = await fetch("http://localhost:3000/friends.json");
     const data = await res.json();
     return data;
 }
@@ -52,19 +52,6 @@ const FriendCart = async () => {
                         </Link>)
                     }
 
-                    <div className="bg-white rounded-lg shadow-md p-4 flex items-center flex-col gap-2">
-                        <Image
-                            src="https://randomuser.me/api/portraits/men/14.jpg"
-                            alt="John Doe"
-                            width={100}
-                            height={100}
-                            className="rounded-full object-cover object-center w-[100px] h-[100px]"
-                        />
-                        <h3 className="text-xl font-semibold mb-2">John Doe</h3>
-                        <p className="text-gray-600">62d ago</p>
-                        <button className="bg-[#CBFADB]  text-[#244D3F]  py-2 px-2 rounded-full text-sm">WORK</button>
-                        <button className="bg-[#EFAD44]  text-white  py-2 px-2 rounded-full text-sm">Almost Due</button>
-                    </div>
                 </div>
 
             </div>
